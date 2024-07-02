6b9148e632e1fced4ccffd27dc1cd9d0142b499b
Jeffrich <jeff.richard@fugro.com>
2024-07-02 15:15:02 -0500

    Finalized

--- ReadTextBox.py
+++ ReadTextBox.py
@@ -9,7 +9,7 @@
 # Required Packages:    PyQt5, PyQt5-Tools
 # -------------------------------------------------------------------------------
 
-from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QAction
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QAction, QLineEdit, QLabel
 from PyQt5 import uic
 import sys
 
@@ -24,14 +24,23 @@ class UI(QMainWindow):
         # define Widgets
         self.btnExit = self.findChild(QPushButton, "btnExit")
         self.actExit = self.findChild(QAction, "actExit")
+        self.txtInputBox = self.findChild(QLineEdit, "txtInputBox")
+        self.btnGo = self.findChild(QPushButton, "btnGo")
+        self.label = self.findChild(QLabel, "label")
 
         # Define the actions
         self.btnExit.clicked.connect(self.closeEvent)
         self.actExit.triggered.connect(self.closeEvent)
+        self.btnGo.clicked.connect(self.print_label)
 
         # Show the app
         self.show()
 
+    def print_label(self):
+        print("Will print to label")
+        print(self.txtInputBox.text())
+        self.label.setText(str(self.txtInputBox.text()))
+    
     def closeEvent(self, *args, **kwargs):
         # print("Program closed Successfully!")
         self.close()
